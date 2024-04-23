1b843e06b4a2a87cd25cae2bf3e530555f2bcbb2
Dipankar Maikap <45673791+dipankarmaikap@users.noreply.github.com>
2024-04-23 20:06:48 +0530

    Live preview working

--- src/utils/setupStoryblokLiveEditor.js
+++
@@ -1,8 +0,0 @@
-import handleStoryblokMessage from './handleStoryblokMessage'
-
-export default function setupStoryblokLiveEditor() {
-  if (window?.storyblokRegisterEvent !== undefined) {
-    console.log(window['storyblokRegisterEvent'])
-  }
-  window.addEventListener('message', handleStoryblokMessage, false)
-}
